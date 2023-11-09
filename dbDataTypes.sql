9d263685d61d44a810bbc6518966f39d93ddac3b
HeverttonNoliveira <oliveirahn.nunes@gmail.com>
2023-11-09 09:54:30 -0300

    Aula 09/11

--- dbDataTypes.sql
+++ dbDataTypes.sql
@@ -22,7 +22,7 @@ show tables;
 -- visualizar a tabela
 desc tbPet;
 desc tbUsuarios;
-
+-
 -- Insirido registro nas tabelas
 INSERT INTO tbPet (Codigo,Nome,Email,Telefone,cpf,dataEnt,salario)
 values(1,'Abigail Oliveira','abigail@hotmail.com','55555-5555','555.555.555-55','2005/01/10',1500.65);
@@ -35,5 +35,4 @@ values(3,'Arthur','athur@gmail.com','66666-6666','222.222.222-22','2005/01/10',2
 
 -- visualizando os registro/dados insiridos
 Select * from tbPet;
-
 
